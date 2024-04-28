00441975ac2c162f86ad69bcc2a84f24c25cb5fa
yago <yagosonicmassaroni@gmail.com>
2024-04-27 22:26:51 -0300

    :lipstick: finalizado a parte visual de adicionar task

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
-import './App.css'
+import styles from "./App.module.css"
 import { Header } from './components/Header'
 import { Input } from './components/Input'
+import { Button } from './components/Button'
 
 function App() {
 
   return (
     <>
       <Header />
-      <Input/>
+      <section className={styles.container}>
+      <div className={styles.taskInfo}>
+        <Input />
+        <Button />
+      </div>
+
+      <div>
+        
+      </div>
+      </section>
     </>
   )
 }
---
+++ src/components/List/ListHeader.jsx
@@ -0,0 +1,5 @@
+import styles from "./ListHeader.module.css"  
+
+export function ListHeader() {
+
+ }
